7ff4570e479ad356c8625e3b27432fcb68c1eae9
MaxDeBenedetti <modsme@gmail.com>
2014-09-13 16:16:06 -0500

    things is workin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,30 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	public Thought[] thoughts;
+	public float minWait, maxWait, impatient;
+	public bool gameplaying = true;
+	private float wait;
 	public Transform[] spawnPoints;
 	// Use this for initialization
 	void Start () {
-	
+		wait = maxWait;
+		StartCoroutine("distract");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		SpawnThought();
+
+
+	}
+
+	IEnumerator distract(){
+		while(gameplaying){
+			if(minWait < wait){
+				wait -= impatient;
+			}
+			SpawnThought();
+			yield return new WaitForSeconds(wait);
+		}
 	}
 
 	void SpawnThought(){
