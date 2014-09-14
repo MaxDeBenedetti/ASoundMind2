55c3c4ad8a6976d8466486a1e4c46f3b05862416
MaxDeBenedetti <modsme@gmail.com>
2014-09-13 20:30:58 -0500

    better invterval delay

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,20 +4,40 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
 	public Thought[] thoughts;
-	public float minWait, maxWait, impatient;
+	public float minWait, maxWait, impatient, interval, loseItChance, maxWaitDecrease;
+	public static int score, sADD;
 	public bool gameplaying = true;
+	private bool isCrazy = false;
 	private float wait;
 	public Transform[] spawnPoints;
 	// Use this for initialization
 	void Start () {
 		wait = maxWait;
-		StartCoroutine("distract");
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
 
+	IEnumerator thinkHarder(){
+		while(maxWait > minWait){
+			if(Random.value < loseItChance){
+				wait = 0;
+			}
+			if(isCrazy){
+				isCrazy = !isCrazy;
+				wait = maxWait;
+				yield return  new WaitForSeconds(interval);
+			}
+			else if(wait < minWait){
+				isCrazy = true;
+				maxWait -= maxWaitDecrease;
+				wait = minWait;
+				float temp = .5f + Random.value*1.5f;
+				yield return  new WaitForSeconds(temp);
+			}
+			else{
+				wait -= impatient;
+				yield return  new WaitForSeconds(interval);
+			}
+		}
 	}
 
 	IEnumerator distract(){
@@ -34,5 +54,13 @@ public class GameManager : MonoBehaviour {
 		Vector3 spawnplace = spawnPoints[(int)UnityEngine.Random.Range(0,spawnPoints.Length)].position;
 		Thought thought = thoughts[(int)UnityEngine.Random.Range(0,thoughts.Length)];
 		GameObject.Instantiate(thought,spawnplace,Quaternion.identity);
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 }
